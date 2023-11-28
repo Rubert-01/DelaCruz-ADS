0cbd01d9ba15fdff220e723c7b45551f76856bc3
Rubert-01 <149487853+Rubert-01@users.noreply.github.com>
2023-11-28 20:38:54 +0800

    Minor Changes
    opposite file names and some adjustment

--- functions/add.php
+++ functions/add.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Add Data</title>
     </head>
     <body>
@@ -36,3 +34,7 @@
         ?>
     </body>
 </html>
+
+
+
+
